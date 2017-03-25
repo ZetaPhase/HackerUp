66e261a158a0ea329ff699f1f132adfc751e9773
0xFireball <0xFireball@outlook.com>
2017-03-25 14:35:30 -0700

    add hosting config

--- server/HackerUp.Server/Program.cs
+++ server/HackerUp.Server/Program.cs
@@ -1,6 +1,8 @@
 ﻿
 using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
 
 namespace HackerUp.Server
 {
@@ -8,9 +10,17 @@ namespace HackerUp.Server
     {
         public static void Main(string[] args)
         {
+
+            // Load ASP.NET Core web app
+            var config = new ConfigurationBuilder()
+                .AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(), "hosting.json"), true)
+                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+                .Build();
+
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseConfiguration(config)
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();

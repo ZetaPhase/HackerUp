9563b96b97bd70e8df0fd017d23a26e0a4e26864
daveho8888 <daveho8888@gmail.com>
2017-03-25 16:33:40 -0700

    make sure to httppost register when starting up app even if apikey exists

--- app/android/HackerUp/app/src/main/java/io/zetaphase/hackerup/MainActivity.java
+++ app/android/HackerUp/app/src/main/java/io/zetaphase/hackerup/MainActivity.java
@@ -9,9 +9,18 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,8 +45,8 @@ public class MainActivity extends AppCompatActivity {
                 double latitude = location.getLatitude();
                 JSONObject position = new JSONObject();
                 try {
-                    position.put("lat", latitude);
-                    position.put("lng", longitude);
+                    position.put("latitude", latitude);
+                    position.put("longitude", longitude);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -54,5 +63,49 @@ public class MainActivity extends AppCompatActivity {
 
     void stopRepeatingTask() {
         mHandler.removeCallbacks(mStatusChecker);
+    }
+
+    private String[] request(String urlString, JSONObject jsonObj) {
+        // TODO Auto-generated method stub
+
+        StringBuffer chaine = new StringBuffer("");
+        int code = -1;
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestProperty("User-Agent", "");
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-type", "application/json");
+            connection.setDoInput(true);
+            connection.connect();
+
+
+            Log.d("REQUESTOUTPUT", "requesting");
+            byte[] b = jsonObj.toString().getBytes();
+            OutputStream outputStream = connection.getOutputStream();
+            outputStream.write(b);
+
+            code = connection.getResponseCode();
+
+            InputStream inputStream = connection.getInputStream();
+
+            BufferedReader rd = new BufferedReader(new InputStreamReader(inputStream));
+            String line = "";
+            while ((line = rd.readLine()) != null) {
+                chaine.append(line);
+            }
+
+        } catch (IOException e) {
+            // writing exception to log
+            e.printStackTrace();
+        }
+
+        Log.d("STATUSCODE", ""+code);
+
+        String result[] = new String[2];
+        result[0] = ""+code;
+        result[1] = chaine.toString();
+
+        return result;
     }
 }
--- app/android/HackerUp/app/src/main/java/io/zetaphase/hackerup/SplashScreen.java
+++ app/android/HackerUp/app/src/main/java/io/zetaphase/hackerup/SplashScreen.java
@@ -4,6 +4,17 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Created by Dave Ho on 3/14/2017.
@@ -27,5 +38,49 @@ public class SplashScreen extends Activity{
                 finish();
             }
         }, SPLASH_TIME_OUT);
+    }
+
+    private String[] request(String urlString, JSONObject jsonObj) {
+        // TODO Auto-generated method stub
+
+        StringBuffer chaine = new StringBuffer("");
+        int code = -1;
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestProperty("User-Agent", "");
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-type", "application/json");
+            connection.setDoInput(true);
+            connection.connect();
+
+
+            Log.d("REQUESTOUTPUT", "requesting");
+            byte[] b = jsonObj.toString().getBytes();
+            OutputStream outputStream = connection.getOutputStream();
+            outputStream.write(b);
+
+            code = connection.getResponseCode();
+
+            InputStream inputStream = connection.getInputStream();
+
+            BufferedReader rd = new BufferedReader(new InputStreamReader(inputStream));
+            String line = "";
+            while ((line = rd.readLine()) != null) {
+                chaine.append(line);
+            }
+
+        } catch (IOException e) {
+            // writing exception to log
+            e.printStackTrace();
+        }
+
+        Log.d("STATUSCODE", ""+code);
+
+        String result[] = new String[2];
+        result[0] = ""+code;
+        result[1] = chaine.toString();
+
+        return result;
     }
 }

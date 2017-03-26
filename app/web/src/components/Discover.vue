b8cf652e18f055406f1c446deafb02e3d0d981f2
0xFireball <0xFireball@outlook.com>
2017-03-26 01:45:01 -0700

    use glob matching

--- app/web/src/components/Discover.vue
+++ app/web/src/components/Discover.vue
@@ -4,13 +4,33 @@
 </template>
 
 <script>
-export default {
-  name: 'discover',
-  data () {
-    return {
+  import axios from 'axios'
+
+  export default {
+    name: 'discover',
+    data () {
+      return {
+      }
+    },
+    methods: {
+      refreshNearby: function () {
+        console.log('refreshing nearby')
+        let vm = this
+        let rfU = '/a/k/nearby/150000?apikey=' + vm.$root.u.key
+        console.log(rfU)
+        axios.get(rfU)
+          .then(function (response) {
+            // todo
+            console.log(response.data)
+            setTimeout(vm.refreshNearby, 2000)
+          })
+      }
+    },
+    mounted: function () {
+      let vm = this
+      setTimeout(vm.refreshNearby, 500)
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->

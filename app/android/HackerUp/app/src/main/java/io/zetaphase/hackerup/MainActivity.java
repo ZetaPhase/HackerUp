59f176388b64ced5acae71ee44786a03fba51032
daveho8888 <daveho8888@gmail.com>
2017-03-25 17:26:15 -0700

    finish ping

--- app/android/HackerUp/app/src/main/java/io/zetaphase/hackerup/MainActivity.java
+++ app/android/HackerUp/app/src/main/java/io/zetaphase/hackerup/MainActivity.java
@@ -2,6 +2,7 @@ package io.zetaphase.hackerup;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
@@ -10,6 +11,7 @@ import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -21,10 +23,16 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     Handler mHandler;
+    private String serverAddress = "192.168.43.198:5000";
+    int statusCode;
+    boolean pingSuccess = true;
+    String MYPREFERENCES = "HACKERUP";
+    LocationManager mLocationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,24 +46,76 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void run() {
             try {
+                Log.d("PING", "pinging");
                 // TODO: Ping server here
-                LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-                Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                double longitude = location.getLongitude();
-                double latitude = location.getLatitude();
-                JSONObject position = new JSONObject();
-                try {
-                    position.put("latitude", latitude);
-                    position.put("longitude", longitude);
-                } catch (JSONException e) {
-                    e.printStackTrace();
+                Location myLocation = getLastKnownLocation();
+                final double latitude = myLocation.getLatitude();
+                final double longitude = myLocation.getLongitude();
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        JSONObject position = new JSONObject();
+                        try {
+                            position.put("latitude", latitude);
+                            position.put("longitude", longitude);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        Log.d("OBJECT", position.toString());
+                        SharedPreferences sharedpreferences = getSharedPreferences(MYPREFERENCES, Context.MODE_PRIVATE);
+                        String apikey = sharedpreferences.getString("ApiKey", "null");
+                        int a = request("http://" + serverAddress + "/k/ping?apikey=" + apikey, position);
+
+                        setStatusCode(a);
+                        Log.d("STATUSCODE", "" + getStatusCode());
+                    }
+                });
+
+                thread.start();
+                thread.join();
+
+                if (getStatusCode() == 200) {
+                    pingSuccess = true;
+                } else {
+                    pingSuccess = false;
+                    Toast.makeText(MainActivity.this, "Stuff is bad.", Toast.LENGTH_SHORT);
                 }
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             } finally {
                 //repeat this runnable
                 mHandler.postDelayed(mStatusChecker, 15000);
             }
         }
     };
+
+    private Location getLastKnownLocation() {
+        mLocationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+        List<String> providers = mLocationManager.getProviders(true);
+        Location bestLocation = null;
+        for (String provider : providers) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return null;
+            }
+            Location l = mLocationManager.getLastKnownLocation(provider);
+            if (l == null) {
+                continue;
+            }
+            if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy()) {
+                // Found best last known location: %s", l);
+                bestLocation = l;
+            }
+        }
+        return bestLocation;
+    }
 
     void startRepeatingTask() {
         mStatusChecker.run();
@@ -65,7 +125,10 @@ public class MainActivity extends AppCompatActivity {
         mHandler.removeCallbacks(mStatusChecker);
     }
 
-    private String[] request(String urlString, JSONObject jsonObj) {
+    public void setStatusCode(int statusCode){ this.statusCode = statusCode; }
+    public int getStatusCode(){ return this.statusCode; }
+
+    private int request(String urlString, JSONObject jsonObj) {
         // TODO Auto-generated method stub
 
         StringBuffer chaine = new StringBuffer("");
@@ -102,10 +165,6 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d("STATUSCODE", ""+code);
 
-        String result[] = new String[2];
-        result[0] = ""+code;
-        result[1] = chaine.toString();
-
-        return result;
+        return code;
     }
 }
